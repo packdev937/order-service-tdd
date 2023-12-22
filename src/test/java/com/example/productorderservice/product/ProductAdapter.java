6b7734c4be80ab6a283aad5ce960a42800038e7d
gundorit <packdev937@gmail.com>
2023-12-23 00:09:04 +0900

    feat (ProductRepository) : Product를 저장할 Repository를 생성한다

---
+++ src/test/java/com/example/productorderservice/product/ProductAdapter.java
@@ -0,0 +1,17 @@
+package com.example.productorderservice.product;
+
+
+public class ProductAdapter implements ProductPort {
+
+    private final ProductRepository productRepository;
+
+    public ProductAdapter(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public void save(Product product) {
+        productRepository.save(product);
+    }
+
+}
---
+++ src/test/java/com/example/productorderservice/product/ProductPort.java
@@ -0,0 +1,6 @@
+package com.example.productorderservice.product;
+
+interface ProductPort {
+
+    public void save(final Product product);
+}
