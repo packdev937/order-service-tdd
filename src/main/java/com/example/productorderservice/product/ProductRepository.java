8870b8b6271de5e66c663fa55b00ad04cfc5fd6c
gundorit <packdev937@gmail.com>
2023-12-25 22:50:22 +0900

    refactor (ProductRepository) : JPARepository를 상속하도록 리팩토링 한다

--- src/main/java/com/example/productorderservice/product/ProductRepository.java
+++ src/main/java/com/example/productorderservice/product/ProductRepository.java
@@ -2,16 +2,10 @@ package com.example.productorderservice.product;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-class ProductRepository {
+interface ProductRepository extends JpaRepository<Product, Long> {
 
-    private Map<Long, Product> persistence = new HashMap<>();
-    private Long sequence = 0L;
-
-    public void save(final Product product) {
-        product.assignId(++sequence);
-        persistence.put(product.getId(), product);
-    }
 }
