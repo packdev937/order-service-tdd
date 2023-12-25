a52457094ff7aab065313d405abf14b7db2b1b32
gundorit <packdev937@gmail.com>
2023-12-25 22:50:48 +0900

    refactor (ProductService) : @Transactional을 추가한다

--- src/main/java/com/example/productorderservice/product/ProductService.java
+++ src/main/java/com/example/productorderservice/product/ProductService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ class ProductService {
     private final ProductPort productPort;
 
     @PostMapping("/products")
+    @Transactional
     public ResponseEntity<Void> addProduct(@RequestBody AddProductRequest request) {
         Product product = new Product(request.name(), request.price(), request.discountPolicy());
         productPort.save(product);
