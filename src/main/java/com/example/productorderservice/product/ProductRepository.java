8bbdf16637743161ee5fd8c05d17e2495394d3d0
Geonwoo Pack <packdev937@gmail.com>
2023-12-28 16:53:38 +0900

    feat (DatabaseCleanup) : 테스트 격리를 위해 DatabaseCleanup을 추가한다

--- src/main/java/com/example/productorderservice/product/ProductRepository.java
+++ src/main/java/com/example/productorderservice/product/ProductRepository.java
@@ -1,7 +1,5 @@
 package com.example.productorderservice.product;
 
-import java.util.HashMap;
-import java.util.Map;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
