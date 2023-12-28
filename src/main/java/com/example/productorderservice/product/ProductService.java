756e6aaab32c437061e1c72e53dc8094bd36cd35
Geonwoo Pack <packdev937@gmail.com>
2023-12-28 19:26:57 +0900

    refactor (ProductApiTest) : API 테스트로 리팩토링 한다

--- src/main/java/com/example/productorderservice/product/ProductService.java
+++ src/main/java/com/example/productorderservice/product/ProductService.java
@@ -5,18 +5,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Service
 @RequiredArgsConstructor
+@RequestMapping("/products")
 class ProductService {
 
     private final ProductPort productPort;
 
-    @PostMapping("/products")
+    @PostMapping()
     @Transactional
     public ResponseEntity<Void> addProduct(@RequestBody AddProductRequest request) {
         Product product = new Product(request.name(), request.price(), request.discountPolicy());
@@ -25,9 +29,12 @@ class ProductService {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    public GetProductResponse getProduct(Long productId) {
+    @GetMapping("/{productId}")
+    public ResponseEntity<GetProductResponse> getProduct(@PathVariable Long productId) {
         Product product = productPort.getProduct(productId);
 
-        return new GetProductResponse(product.getId(), product.getName(), product.getPrice(), product.getDiscountPolicy());
+        GetProductResponse response = new GetProductResponse(product.getId(),
+            product.getName(), product.getPrice(), product.getDiscountPolicy());
+        return ResponseEntity.ok(response);
     }
 }
--- src/test/java/com/example/productorderservice/product/ProductApiTest.java
+++ src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -3,10 +3,15 @@ package com.example.productorderservice.product;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import com.example.productorderservice.ApiTest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
 public class ProductApiTest extends ApiTest {
+
+    private AddProductRequest request;
 
     @Test
     void 상품등록() {
@@ -16,4 +21,18 @@ public class ProductApiTest extends ApiTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
+    @Test
+    void 상품조회() {
+        ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
+        Long productId = 1L;
+
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+            .when()
+            .get("/products/{productId}", productId)
+            .then().log().all()
+            .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getString("name")).isEqualTo("상품명");
+    }
 }
