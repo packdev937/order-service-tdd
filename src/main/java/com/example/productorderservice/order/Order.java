a89aba865d20de7df12a7360632f3d998b9d575d
Geonwoo Pack <packdev937@gmail.com>
2023-12-29 20:31:46 +0900

    refactor (Order) : JPA Repository를 사용하도록 리팩토링 한다

--- src/main/java/com/example/productorderservice/order/Order.java
+++ src/main/java/com/example/productorderservice/order/Order.java
@@ -1,34 +1,35 @@
 package com.example.productorderservice.order;
 
 import com.example.productorderservice.product.Product;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 class Order {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private final Product product;
-    private final int quantity;
 
-    public Order(Product product, int quantity) {
+    @OneToOne
+    private Product product;
+    private int quantity;
+
+    public Order(final Product product, final int quantity) {
         Assert.notNull(product, "상품은 필수입니다.");
         Assert.isTrue(quantity > 0, "상품 수량은 0보다 커야 합니다.");
         this.product = product;
         this.quantity = quantity;
-    }
-
-    public void assignId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public int getQuantity() {
-        return quantity;
     }
 }
--- src/main/java/com/example/productorderservice/order/OrderRepository.java
+++ src/main/java/com/example/productorderservice/order/OrderRepository.java
@@ -1,17 +1,7 @@
 package com.example.productorderservice.order;
 
-import java.util.HashMap;
-import java.util.Map;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-class OrderRepository {
+interface OrderRepository extends JpaRepository<Order, Long> {
 
-    private final Map<Long, Order> persistance = new HashMap<>();
-    private Long sequence = 0L;
-
-    public void save(final Order order) {
-        order.assignId(sequence++);
-        persistance.put(order.getId(), order);
-    }
 }
